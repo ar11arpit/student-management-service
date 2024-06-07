dc381aed7a9b30c199f5a5f39437f86b467480c5
ar11arpit <84989550+ar11arpit@users.noreply.github.com>
2024-06-07 12:11:33 +0530

    Update StudentService.java

--- ms-student-service/src/main/java/com/rakbank/student/service/StudentService.java
+++ ms-student-service/src/main/java/com/rakbank/student/service/StudentService.java
@@ -1,5 +1,5 @@
 package com.rakbank.student.service;
-
+import java.util.List;
 import com.rakbank.student.entity.Student;
 import com.rakbank.student.response.StudentResponse;
 
@@ -7,4 +7,6 @@ public interface StudentService {
     StudentResponse getStudentByStudentId(String studentId);
 
     StudentResponse addStudent(Student student) ;
+
+    List<Student> getStudentDetials();
 }
